33a6cd70284bcb4db3751d9f8761852420b71e1e
a-regal <andresregal97@gmail.com>
2021-12-01 12:19:00 -0500

    Add final solution

--- problem_1/src/main.rs
+++ problem_1/src/main.rs
@@ -6,16 +6,38 @@ pub fn read_to_array(filename: &str) -> Vec<i64>{
     return measurements;
 }
 
-
-fn main() {
-    let measures = read_to_array("input.txt");
-    
+pub fn part_1(input: &Vec<i64>) -> i64{
     let mut increases = 0;
-    for i in 1..measures.len(){
-        if measures[i] > measures[i-1] {
+    for i in 1..input.len(){
+        if input[i] > input[i-1] {
             increases += 1;
         }
     }
+    return increases;
    
-    println!("{} increases", increases)
+}
+
+pub fn part_2(input: &Vec<i64>) -> i64{
+    let mut increases = 0;
+
+    //Same as part 1 but with a 3-window
+    for i in 0..input.len()-3{
+        let win_1 = input[i] + input[i+1] + input[i+2];
+        let win_2 = input[i+1] + input[i+2] + input[i+3];
+
+        if win_2 > win_1{
+            increases += 1;
+        }
+    }
+
+    return increases;
+}
+
+fn main() {
+    let measures = read_to_array("input.txt");
+
+    let part_a_sol = part_1(&measures);
+    let part_b_sol = part_2(&measures);
+    println!("{} increases", part_a_sol);
+    println!("{} increaes with 3-window", part_b_sol);
 }
