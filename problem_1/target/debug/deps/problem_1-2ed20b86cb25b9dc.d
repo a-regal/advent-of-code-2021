620e8cc5d3347242b69dc9ae6a47a7f187ed728a
a-regal <andresregal97@gmail.com>
2021-12-01 11:43:23 -0500

    Initial commit for problem 1

---
+++ problem_1/target/debug/deps/problem_1-2ed20b86cb25b9dc.d
@@ -0,0 +1,5 @@
+/Users/andresregal/Documents/personal/advent-of-code-2021/problem_1/target/debug/deps/problem_1-2ed20b86cb25b9dc.rmeta: src/main.rs
+
+/Users/andresregal/Documents/personal/advent-of-code-2021/problem_1/target/debug/deps/problem_1-2ed20b86cb25b9dc.d: src/main.rs
+
+src/main.rs:
