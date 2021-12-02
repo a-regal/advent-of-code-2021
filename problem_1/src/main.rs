ab9f0eea5cdcad0ac38566b575a41117c4547b11
a-regal <andresregal97@gmail.com>
2021-12-01 19:17:50 -0500

    Remove pub function signature

--- problem_1/src/main.rs
+++ problem_1/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-pub fn read_to_array(filename: &str) -> Vec<i64>{
+fn read_to_array(filename: &str) -> Vec<i64>{
     let content = fs::read_to_string(filename).expect("Reading error");
     let measurements: Vec<i64> = content.trim().split("\n").map(|l| l.parse().unwrap()).collect();
     return measurements;
 }
 
-pub fn part_1(input: &Vec<i64>) -> i64{
+fn part_1(input: &Vec<i64>) -> i64{
     let mut increases = 0;
     for i in 1..input.len(){
         if input[i] > input[i-1] {
@@ -14,10 +14,9 @@ pub fn part_1(input: &Vec<i64>) -> i64{
         }
     }
     return increases;
-   
 }
 
-pub fn part_2(input: &Vec<i64>) -> i64{
+fn part_2(input: &Vec<i64>) -> i64{
     let mut increases = 0;
 
     //Same as part 1 but with a 3-window
@@ -29,13 +28,11 @@ pub fn part_2(input: &Vec<i64>) -> i64{
             increases += 1;
         }
     }
-
     return increases;
 }
 
 fn main() {
     let measures = read_to_array("input.txt");
-
     let part_a_sol = part_1(&measures);
     let part_b_sol = part_2(&measures);
     println!("{} increases", part_a_sol);
